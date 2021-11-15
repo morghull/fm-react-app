ad2440e61ba3a1030e28bc81f2b06bf25542a0ab
morghull <morghull@gmail.com>
2021-11-15 19:10:47 +0200

    add sorting

--- src/App.js
+++ src/App.js
@@ -7,25 +7,42 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isStraight: true,
       users: [
         { id: 1, fname: 'ELon' },
         { id: 2, fname: 'Elen' },
         { id: 3, fname: 'Andre' },
         { id: 4, fname: 'Tim' },
-        { id: 5, fname: 'Lee' },
+        { id: 9, fname: 'Lee' },
+        { id: 5, fname: 'Lao' },
       ],
     };
   }
+  sortUsers = () => {
+    const { users, isStraight } = this.state;
+    const sortUsers = JSON.parse(JSON.stringify(users));
+    sortUsers.sort((prev, next) =>
+      isStraight ? next.id - prev.id : prev.id - next.id
+    );
+    this.setState({
+      isStraight: !isStraight,
+      users: sortUsers,
+    });
+  };
   render() {
-    const { users } = this.state;
+    const { users, isStraight } = this.state;
     return (
-      <ul>
-        {users.map(({ fname, id }) => (
-          <li>
-            <Ciao key={id} name={fname} />
-          </li>
-        ))}
-      </ul>
+      <>
+        <p>{isStraight ? 'straight' : 'reverse'}</p>
+        <button onClick={this.sortUsers}>sort</button>
+        <ul>
+          {users.map(({ fname, id }, i) => (
+            <li key={i}>
+              <Ciao key={id} name={fname} />
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 }
