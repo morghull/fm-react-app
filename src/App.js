815a91bf6177406687f12927d12497210ab45921
morghull <morghull@gmail.com>
2021-11-15 19:53:40 +0200

    add sort by name

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isAlphabet: true,
       isStraight: true,
       users: [
         { id: 1, fname: 'ELon' },
@@ -18,24 +19,51 @@ class App extends Component {
       ],
     };
   }
-  sortUsers = () => {
-    const { users, isStraight } = this.state;
+  sortUsersById = () => {
+    const { users, isStraight, isAlphabet } = this.state;
     const sortUsers = JSON.parse(JSON.stringify(users));
     sortUsers.sort((prev, next) =>
       isStraight ? next.id - prev.id : prev.id - next.id
     );
     this.setState({
+      isAlphabet: isAlphabet,
       isStraight: !isStraight,
       users: sortUsers,
     });
   };
+  sortUsersByName = () => {
+    const { users, isStraight, isAlphabet } = this.state;
+    const sortUsers = JSON.parse(JSON.stringify(users));
+    sortUsers.sort((prev, next) =>
+      isAlphabet
+        ? next.fname > prev.fname
+          ? 1
+          : next.fname < prev.fname
+          ? -1
+          : 0
+        : prev.fname > next.fname
+        ? 1
+        : prev.fname < next.fname
+        ? -1
+        : 0
+    );
+    this.setState({
+      isAlphabet: !isAlphabet,
+      isStraight: isStraight,
+      users: sortUsers,
+    });
+  };
   render() {
-    const { users, isStraight } = this.state;
+    const { users, isStraight, isAlphabet } = this.state;
     return (
       <>
         <p>
-          {isStraight ? 'straight' : 'reverse'}
-          <button onClick={this.sortUsers}>sort</button>
+          <button onClick={this.sortUsersById}>
+            sort by number {isStraight ? 'straight' : 'reverse'}
+          </button>
+          <button onClick={this.sortUsersByName}>
+            sort by name {isAlphabet ? 'straight' : 'reverse'}
+          </button>
         </p>
         <ul>
           {users.map(({ fname, id }) => (
