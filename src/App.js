929af12f8807296308a3afa856b3424ad2b0e4e3
morghull <morghull@gmail.com>
2021-11-16 01:42:08 +0200

    add UsersList

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-import CiaoSection from './components/CiaoSection';
-
-const userDB = [
-  { id: 1, fname: 'ELon' },
-  { id: 2, fname: 'Elen' },
-  { id: 3, fname: 'Andre' },
-  { id: 4, fname: 'Tim' },
-  { id: 9, fname: 'Lee' },
-  { id: 5, fname: 'Lao' },
-  { id: 6, fname: 'Laod' },
-];
+import UsersList from './components/UsersList';
 
 function App() {
-  return <>{/* <CiaoSection users={userDB} /> */}</>;
+  return (
+    <>
+      <UsersList />
+    </>
+  );
 }
 
 export default App;
--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
-class Index extends Component {
+const usersDB = [
+  { id: 1, fname: 'ELon' },
+  { id: 2, fname: 'Elen' },
+  { id: 3, fname: 'Andre' },
+  { id: 4, fname: 'Tim' },
+  { id: 9, fname: 'Lee' },
+  { id: 5, fname: 'Lao' },
+  { id: 6, fname: 'Laod' },
+];
+
+class UsersList extends Component {
   constructor(props) {
     super(props);
+    this.state = { users: usersDB };
   }
   render() {
-    return <div></div>;
+    const { users } = this.state;
+    return (
+      <section>
+        <h1>List of users:</h1>
+        {users.map(({ id, fname }) => (
+          <article>
+            <h2>
+              <span>{id})</span> {fname}
+            </h2>
+          </article>
+        ))}
+      </section>
+    );
   }
 }
 
-export default Index;
+export default UsersList;
