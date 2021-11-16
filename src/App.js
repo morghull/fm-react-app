082efc2938ed256278bda77e0b9fabeb85288732
morghull <morghull@gmail.com>
2021-11-16 21:25:04 +0200

    add counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import React from 'react';
-import UserSection from './components/UserSection';
+import React,{Component,useState} from 'react';
+import StopWatch from './components/StopWatch';
 
-function App() {
-  return <></>;
+function App(props) {
+  const [isVisible, setVisible] = useState(true);
+  return (
+    <>
+      <button onClick={() => setVisible(!isVisible)}>switch</button>
+      {isVisible ? <StopWatch /> : null}
+    </>
+  );
 }
 
 export default App;
