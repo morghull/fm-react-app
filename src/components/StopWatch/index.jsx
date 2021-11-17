54bb05d3e18d5808499cf183efc9bb4d6c67d776
morghull <morghull@gmail.com>
2021-11-17 19:58:17 +0200

    fix import of syle file after renaming

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from './stop-watch.module.css';
+import styles from './StopWatch.module.css';
 
 class StopWatch extends Component {
   constructor(props) {
