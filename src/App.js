d9e11f0aa8a1b6d94f9482101660b5cc634ca6b7
morghull <morghull@gmail.com>
2021-11-19 10:45:48 +0200

    add more proptypes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { Component, useState } from 'react';
 import Ciao from './components/CiaoSection/Ciao';
 import UserCard from './components/UserCard';
 import UsersList from './components/UsersList';
+import CiaoList from './components/CiaoSection/CiaoList';
 
 function App(props) {
   const user = {
     id: 1,
     name: 'Elen',
+    fname: 'Elon',
   };
   const user1 = {
     id: 1,
@@ -21,6 +23,7 @@ function App(props) {
   return (
     <>
       <Ciao name={user.name} id={user.id} />
+      <CiaoList users={[user]}/>
       <UserCard />
       <UsersList users={[user1,user2]}/>
     </>
