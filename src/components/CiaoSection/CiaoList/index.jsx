5b8b3d1557ed22d16f632d98ddc39baa2c9c8cbc
morghull <morghull@gmail.com>
2021-11-19 10:46:03 +0200

    more proptypes

--- src/components/CiaoSection/CiaoList/index.jsx
+++ src/components/CiaoSection/CiaoList/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Ciao from '../Ciao';
 
+/**
+ *
+ * @param {*} props
+ * @param {array} props.users
+ * @returns
+ */
 const CiaoList = (props) => {
   const { users } = props;
   const mapUserCiao = ({ fname, id }) => (
@@ -9,6 +16,15 @@ const CiaoList = (props) => {
     </li>
   );
   return <ul>{users.map(mapUserCiao)}</ul>;
+};
+
+const userShape = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+CiaoList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape(userShape)).isRequired,
 };
 
 export default CiaoList;
